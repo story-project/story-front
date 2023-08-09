e68f979f548359d460aa25a72a3004d036fce7d8
Oybek <oybeksalomov256@gmail.com>
2023-08-09 10:53:22 +0500

    move home to core

---
+++ src/app/core/home/home.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from '@angular/core';
+import {StoryCartService} from "../../story-cart/story-cart.service";
+import {StoryCart} from "../../story-cart/story-cart";
+import {Observable} from "rxjs";
+import {CategoryService} from "../../category/category.service";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit{
+  storyCarts!: any
+  categories!: any
+  constructor(
+    private storyCartService: StoryCartService,
+    private categoryService: CategoryService,
+    ) {
+  }
+
+  ngOnInit() {
+    this.storyCartService.getStories().subscribe((storyCarts: any) => {
+      this.storyCarts = storyCarts['hydra:member']
+      console.log(this.storyCarts)
+    })
+
+    this.categoryService.getCategories().subscribe((categories: any) => {
+      this.categories = categories['hydra:member']
+      console.log(this.categories)
+    })
+  }
+}
