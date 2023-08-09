db43e9197588cf5181893e324a388f5fadb08863
Oybek <oybeksalomov256@gmail.com>
2023-08-09 10:54:20 +0500

    edit login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../auth.service";
+import {Auth} from "../auth";
+import {provideRouter, Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -10,7 +13,11 @@ export class LoginComponent implements OnInit{
   hide = true
   loginForm!: FormGroup
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router) {
+
   }
 
   ngOnInit() {
@@ -18,7 +25,22 @@ export class LoginComponent implements OnInit{
       email: [
         '',
         [Validators.required, Validators.email]
+      ],
+      password: [
+        '',
+        [Validators.required, Validators.minLength(6)]
       ]
     })
+
+  }
+
+  loginProcess() {
+    this.authService.login(this.loginForm.value).subscribe((jwtToken) => {
+      localStorage.setItem('jwtToken', jwtToken.accessToken)
+      this.router.navigate(['/home'])
+    })
+  }
+  get email() {
+    return this.loginForm.controls?.['email']
   }
 }
