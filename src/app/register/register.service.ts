4a22ca1acb62cc0c94a78867d2f53f0722a757c1
Oybek <oybeksalomov256@gmail.com>
2023-08-11 01:08:39 +0500

    edit register

--- src/app/register/register.service.ts
+++ src/app/register/register.service.ts
@@ -2,8 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment";
 import {Register} from "./register";
-import {Person} from "./person";
-import {BehaviorSubject, Observable} from "rxjs";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
